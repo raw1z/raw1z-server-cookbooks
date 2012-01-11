c709df7dc15a1f8993ae0cd2a594f4792ffb5053
Rawane ZOSSOU <dev@raw1z.fr>
2012-01-11 05:11:17 +0100

    added the imagemagick cookbook

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,7 +1,13 @@
+# mysql
 include_recipe "mysql::server"
 
+# rvm
 node['rvm']['default_ruby']      = "ruby-1.9.3-p0"
 node['rvm']['user_default_ruby'] = "ruby-1.9.3-p0"
 include_recipe "rvm::system"
 
+# passenger
 include_recipe "passenger::daemon"
+
+# image magick
+include_recipe "imagemagick::rmagick"
