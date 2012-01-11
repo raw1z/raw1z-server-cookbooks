6654783c57f490661c7644085d33949d56639bc9
Rawane ZOSSOU <dev@raw1z.fr>
2012-01-11 01:56:41 +0100

    updated passenger/nginx install

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,4 +4,6 @@ node['rvm']['default_ruby']      = "ruby-1.9.3-p0"
 node['rvm']['user_default_ruby'] = "ruby-1.9.3-p0"
 include_recipe "rvm::system"
 
-include_recipe "passenger::daemon"
+node['passenger'][:version] = '3.0.11'
+node['passenger'][:nginx][:nginx_version] = "1.0.0"
+include_recipe "passenger::nginx"
