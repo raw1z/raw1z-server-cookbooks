886ae3d8f4f27fd42f8229c59aea50e199956f06
Rawane ZOSSOU <dev@raw1z.fr>
2012-01-11 02:24:48 +0100

    added passenger cookbook

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -4,6 +4,4 @@ node['rvm']['default_ruby']      = "ruby-1.9.3-p0"
 node['rvm']['user_default_ruby'] = "ruby-1.9.3-p0"
 include_recipe "rvm::system"
 
-node['passenger'][:version] = '3.0.11'
-node['passenger'][:nginx][:nginx_version] = "1.0.0"
-include_recipe "passenger::nginx"
+include_recipe "passenger::daemon"
